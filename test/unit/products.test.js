8cccda2356609ef501977ce9166797845a35b7a2
steve <ionc635@naver.com>
2022-03-03 19:51:19 +0900

    add: beforeEach 적용

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -2,18 +2,25 @@ const productController = require("../../controller/product");
 const productModel = require("../../models/product");
 const httpMocks = require("node-mocks-http");
 const newProduct = require("../data/new-product");
+
 productModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe("Product Controller Create", () => {
+  beforeEach(() => {
+    req.body = newProduct;
+  });
+
   it("should have a createProduct function", () => {
     expect(typeof productController.createProduct).toBe("function");
   });
   it("should call productModel.create", () => {
-    let req = httpMocks.createRequest();
-    let res = httpMocks.createResponse();
-    let next = null;
-    req.body = newProduct;
-
     productController.createProduct(req, res, next);
     expect(productModel.create).toBeCalledWith(newProduct);
   });
