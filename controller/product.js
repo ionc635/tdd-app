e7f8d035675fb4631882445c25c04a306c0eafad
steve <ionc635@naver.com>
2022-03-03 23:36:50 +0900

    add: 에러 처리 테스트 작성

--- controller/product.js
+++ controller/product.js
@@ -1,7 +1,10 @@
 const productModel = require("../models/product");
 
 exports.createProduct = async (req, res, next) => {
-  const createdProduct = await productModel.create(req.body);
-  console.log(createdProduct);
-  res.status(201).json(createdProduct);
+  try {
+    const createdProduct = await productModel.create(req.body);
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    next(error);
+  }
 };
